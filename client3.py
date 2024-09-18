c2ff7311348fcec91b3757b41d116fbfb8b2166d
Manish Sharma <147698718+ms5977@users.noreply.github.com>
2024-09-18 13:52:15 +0530

    Update-client3.py

--- client3.py
+++ client3.py
@@ -42,7 +42,11 @@ def getDataPoint(quote):
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    	if (price_b == 0):
+			#when price_b is zero avoid throwing ZeroDevisionError
+			return 
+	return price_a/price_b
+
 
 
 # Main
